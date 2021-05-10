bb304464b9e8edfaa208d8ab8718560766ea878e
sebanti <sebanc99@gmail.com>
2021-05-10 13:00:27 +0530

    Add documentation MaxSumSubarray.java

--- sliding-window/MaxSumSubarray.java
+++ sliding-window/MaxSumSubarray.java
@@ -1,8 +1,9 @@
 /*
 * Find the max sum subarray of a fixed size K
 *
-* Example input:
-* [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+* Example input: [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+*
+* Example output: 16
 *
 */
 
@@ -13,9 +14,14 @@ public class MaxSumSubarray {
         int currentRunningSum = 0;
 
         for (int i = 0; i < A.length; i++) {
+            // keep adding a new element
             currentRunningSum += A[i];
+
+            // if index has reached the sliding window length
             if (i >= k - 1) {
                 maxValue = Math.max(maxValue, currentRunningSum);
+                // remove the element just left of the current window,
+                // having which will violate the window size condition
                 currentRunningSum -= A[i - k + 1];
             }
         }
